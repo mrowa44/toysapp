687a1d1f2cfa7f20718a477d8c39c5df5a900b2b
Justyna Rachowicz <mrowa44@gmail.com>
2015-10-08 17:52:47 +0200

    Add changing room for toy in edit view

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -41,6 +41,7 @@ class ToysController < ApplicationController
   # PATCH/PUT /toys/1.json
   def update
     respond_to do |format|
+      @toy.room_id = params[:toy][:room_id]
       if @toy.update(toy_params)
         format.html { redirect_to @toy, notice: 'Toy was successfully updated.' }
         format.json { render :show, status: :ok, location: @toy }
@@ -69,6 +70,6 @@ class ToysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def toy_params
-      params[:toy]
+      params.require(:toy).permit(:name, :color, :price, :available_num, :room)
     end
 end
