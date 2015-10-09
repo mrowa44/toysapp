15f1f0f9305fe44e435dce43348fff2b05b9d49c
Justyna Rachowicz <mrowa44@gmail.com>
2015-10-09 21:04:49 +0200

    Fix available num of toys for room

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,7 +1,7 @@
 class Toy < ActiveRecord::Base
   default_scope { where active: true }
 
-  belongs_to :room
+  belongs_to :room, touch: true
   has_many :comments, as: :commentable
 
   validates :room_id, presence: true
@@ -9,7 +9,7 @@ class Toy < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 200 }, uniqueness: true
   validates :available_num, numericality: { greater_than: 0, less_than: 1000000 }
 
-  before_create :set_color
+  before_create :set_color, :set_available_toys_num
 
   def destroy
     update(active: false)
@@ -23,7 +23,13 @@ class Toy < ActiveRecord::Base
 
     def set_color
       self.color = 'not defined' if self.color.nil?
-      self.room.number_of_toys += self.available_num
     end
 
+    def set_available_toys_num
+      if self.room.number_of_toys.nil?
+        self.room.number_of_toys = self.available_num
+      else
+        self.room.number_of_toys += self.available_num
+      end
+    end
 end
