b81182bfd49e51cddef0816b06b44f512add197f
Justyna Rachowicz <mrowa44@gmail.com>
2015-10-27 15:24:26 +0100

    Fix toy spec

--- spec/models/toy_spec.rb
+++ spec/models/toy_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Toy, type: :model do
-  it { should validate_presence_of(:room_id) }
+  it { should validate_presence_of(:room_id).on(:save) }
   it { should validate_numericality_of(:price) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(200) }
